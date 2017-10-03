20ccfd37106702404053c91a43cdbed1e6575fc5
Stanislav Sysoev <d4rkr00t@gmail.com>
2017-10-03 16:47:40 +1100

    Use bolt.getProject() to determine project root

--- test.js
+++ test.js
@@ -27,3 +27,20 @@ test('fixture', () => {
     expect(serialize(results)).toMatchSnapshot();
   });
 });
+
+test('fixture from nested folder', () => {
+  return query({
+    cwd: path.join(fixturePath, 'packages', 'bar'),
+    projectFiles: {
+      readme: 'README.md',
+      releases: 'releases/**/*.md',
+    },
+    workspaceFiles: {
+      readme: 'README.md',
+      docs: 'docs/**/*.md',
+      examples: 'examples/**/*.js',
+    },
+  }).then(results => {
+    expect(serialize(results)).toMatchSnapshot();
+  });
+});
